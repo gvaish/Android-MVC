bafa0580da507d8c0cf7e83664512ef966fa2060
Gaurav Vaish <mastergaurav@>
2010-08-23 17:18:29 +0530

    2010-08-23   Gaurav Vaish <mastergaurav@>
    	* HttpGetCommand: Don't override getErrorResponse ATM.

--- android-mvc/codebase/eclipse/src/com/mastergaurav/android/mvc/command/HttpGetCommand.java
+++ android-mvc/codebase/eclipse/src/com/mastergaurav/android/mvc/command/HttpGetCommand.java
@@ -2,25 +2,11 @@ package com.mastergaurav.android.mvc.command;
 
 import java.net.URI;
 
-import org.apache.http.HttpResponse;
-
 public class HttpGetCommand extends AbstractHttpCommand
 {
 	public HttpGetCommand(URI uri)
 	{
 		setURI(uri);
-	}
-
-	@Override
-	protected Object getErrorResponse(HttpResponse response)
-	{
-		return null;
-	}
-
-	@Override
-	protected Object getErrorResponse(Exception error)
-	{
-		return null;
 	}
 
 	@Override
